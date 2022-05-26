1a0558b46178758adc89ac1767a4cf92a3446de5
eric891224 <s96006730@gmail.com>
2022-05-26 09:43:31 +0800

    init

---
+++ Assembly/hw1.s
@@ -0,0 +1,18 @@
+.data
+    n: .word 11
+.text
+.globl __start
+
+FUNCTION:
+    # Todo: Define your own function in HW1
+    
+
+# Do NOT modify this part!!!
+__start:
+    la   t0, n
+    lw   x10, 0(t0)
+    jal  x1,FUNCTION
+    la   t0, n
+    sw   x10, 4(t0)
+    addi a0,x0,10
+    ecall
