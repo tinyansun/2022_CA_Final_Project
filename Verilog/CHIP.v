fd8443637a255d9462c163ce1899251f93648def
tinyansun <patricksun880528@gmail.com>
2022-06-04 23:07:00 +0800

    assign

--- Verilog/CHIP.v
+++ Verilog/CHIP.v
@@ -68,7 +68,11 @@ module CHIP(clk,
     assign rs2 = mem_rdata_I[24 : 20];
     assign rd = mem_rdata_I[11 : 7];
     assign Mem_Read_Write_control = (MemRead_control) ? 0 : 1;  //memory's implementaion requires only either of MemRead_control or MemWrite_control
+	//output
     assign mem_addr_I = PC;
+	assign mem_wen_D = MemWrite_control;
+    assign mem_addr_D = ALU_output;
+    assign mem_wdata_D = rs2;
 	
 	//---------------------------------------//
     // Do not modify this part!!!            //
