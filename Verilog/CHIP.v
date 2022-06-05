904f8c437cccc638dab1e1e1d8fa511f65c687ac
broderick34519 <74870212+broderick34519@users.noreply.github.com>
2022-06-05 23:21:19 +0800

    merge

--- Verilog/CHIP.v
+++ Verilog/CHIP.v
@@ -783,9 +783,6 @@ module ALU(
             4'b1010: begin
                 ALU_output_reg = ALU_input_1 << ALU_input_2;
             end
-            default: begin
-                ALU_output_reg = 0;
-            end
         endcase
     end
 
